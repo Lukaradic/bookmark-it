62f4acad9f0d0efb3c19317db7314802e00132c4
Luka <bezanija1998@gmail.com>
2021-02-07 00:02:28 +0100

    added some url functionality

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 
+let button = document.getElementById('action-button')
+let content = document.querySelector('.content');
+let lstorage = window.localStorage;
+let arr = [];
 
-let button = document.getElementById('button')
-let title = document.getElementById('title')
+(function () {
+    let newArr = JSON.parse(lstorage.getItem('bookmarks'));
+    if (!newArr) arr = [...newArr]
+})()
 
-button.addEventListener('click', e => {
-    title.style.color = '#eee'
+//  get url of current tab 
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    content.textContent = url
+});
+
+button.addEventListener('click', () => {
+
 })
+
+//  onclick add url to the arr 
+//  check if arr is already there
+//  if false add it to arr
+//  render a list made of items in the url arr
